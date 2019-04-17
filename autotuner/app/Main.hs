ed6831bd4ad59e068b81454ea699d8a0c7f15e40
Mathias Graae <C.Mathias.Graae@gmail.com>
2019-04-17 15:25:19 +0200

    Cleaned up and improved output of getTresh

--- autotuner/app/Main.hs
+++ autotuner/app/Main.hs
@@ -6,16 +6,32 @@ import Text.Regex.Base
 import Data.List
 import Data.List.Split
 import Data.Tree
+--import Data.Tree.Pretty
 import Data.Typeable
+import Control.Monad
+import Data.Maybe
 
-getTresh :: String -> [[String]]
-getTresh matches =
-  let mkPattern   = mkRegex "threshold"
-      matchesList = lines matches
-      f = filter (\x -> matchTest mkPattern x) matchesList
+cleanGroups :: [String] -> (String,[String])
+cleanGroups (grp1:grp2:[]) =
+  (grp1 ,tail (splitOn "!main." grp2))
+
+removeAll :: (Eq a) => a -> [[a]] -> [[a]]
+removeAll = map . filter . (/=)
+
+getTresh :: String -> [(String,[String])]
+getTresh sizeOutput = -- Needs to be split up a bit
+  let regex = mkRegex "main\\.(.*)\\ \\(threshold\\ \\((.*)\\)\\)"       
+      outputLines = lines sizeOutput
+      matches = map (matchRegexAll regex) outputLines
+      getGroups = cleanGroups . removeAll ' ' . (\(_,_,_,x) -> x)
+  in  catMaybes $ map (liftM getGroups) matches -- name
+    
+
+  {-
       split = map (\x -> splitOn " " x) f
       f1 = map (\xx -> filter (\x -> not $ matchTest mkPattern x) xx) split
   in reverse . map(\z -> reverse z) . sort $ map (\x -> sort x) f1
+  -}
 
 getName :: String -> String
 getName name =
