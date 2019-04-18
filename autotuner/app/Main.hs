98608a646a0e5ee0fec8543b28ac62c1ad9a6a73
Mathias Graae <C.Mathias.Graae@gmail.com>
2019-04-18 14:27:41 +0200

    Added argument parsing

--- autotuner/app/Main.hs
+++ autotuner/app/Main.hs
@@ -19,14 +19,14 @@ import Data.Maybe
 ------------ regex --------------
 cleanGroups :: [String] -> (String,[String])
 cleanGroups (grp1:grp2:[]) =
-  (grp1 ,tail (splitOn "!main." grp2))
+  (grp1 ,tail (splitOn "!" grp2))
 
 removeAll :: (Eq a) => a -> [[a]] -> [[a]]
 removeAll = map . filter . (/=)
 
 getTresh :: String -> [(String,[String])]
 getTresh sizeOutput = -- Prob needs to be split up a bit
-  let regex = mkRegex "main\\.(.*)\\ \\(threshold\\ \\((.*)\\)\\)"       
+  let regex = mkRegex "(.*)\\ \\(threshold\\ \\((.*)\\)\\)"       
       outputLines = lines sizeOutput
       matches = catMaybes $ map (matchRegexAll regex) outputLines
       getGroups = cleanGroups . removeAll ' ' . (\(_,_,_,x) -> x)
@@ -35,18 +35,43 @@ getTresh sizeOutput = -- Prob needs to be split up a bit
 ------------------
 ------ tree ------
 
-
-treeLevel :: [(String,[String])] -> [String] -> (String, [[String]])
-treeLevel thress [] = 
-  ("nullRoot", roots)
+toTree :: [(String,[String])] -> [String] -> (String, [[String]])
+-- nullRoot is simply to make sure that if no thresholds are found a tree is 
+-- still made
+toTree thress [] = 
+  ("nullRoot", roots) 
   where roots = map ((:[]) . fst) $ filter (null . snd) thress 
-treeLevel thress prevThress =
+toTree thress prevThress =
   (currThres, nextThress)
   where currThres  = head prevThress 
         nextThress = map ((:prevThress) . fst) $ -- Should be it's own function
                      filter (((==) $ sort prevThress) . sort . snd) thress
 
-------------------
+------------------------
+
+----- Parse args -------
+data Flag = None --extend if we create flags
+type Program = String
+type Backend = String
+type Args = (Backend, Program, [Flag])
+
+getBackend :: Args -> Backend
+getBackend (b,_,_) = b
+getProgram :: Args -> Program
+getProgram (_,p,_) = p
+getFlags :: Args -> [Flag]
+getFlags (_,_,f) = f
+
+parseArgs :: [String] -> Args
+parseArgs (backend:program:flags) =
+  (backend, program, parseFlags flags)
+parseArgs _ = error "TODO: Add guide to use here"
+
+parseFlags :: [String] -> [Flag]
+parseFlags [] = [None]
+------------------------
+
+-------- Main ----------
 
 getName :: String -> String
 getName name =
@@ -54,11 +79,14 @@ getName name =
 
 main = do
   args <- getArgs
-  putStrLn $ "futhark " ++ args !! 0 ++ " " ++ args !! 1
-  callCommand $ "futhark " ++ args !! 0 ++ " " ++ args !! 1
-  let name1 = getName $ args !! 1
+  let pArgs = parseArgs args
+  let command = "futhark " ++ getBackend pArgs ++ " " ++ getProgram pArgs
+  putStrLn $ "Compiling with: " ++ command ++ "\n"
+  callCommand command
+  let name1 = getName $ getProgram pArgs
   let name = "./" ++ name1
   sizes <- readProcess name ["--print-sizes"] ""
   let thresh = getTresh sizes
-  let tree = unfoldTree (treeLevel thresh) []
+  let tree = unfoldTree (toTree thresh) []
   putStrLn $ drawTree tree
+-------------------------
